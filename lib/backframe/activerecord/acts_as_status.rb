0a06eebbae5cd6a9c811a757315297efa4ea6ec1
mochini <greg@thinktopography.com>
2016-05-11 08:52:20 -0400

    move acts_as_status to acts_as_enum

--- lib/backframe/activerecord/acts_as_status.rb
+++ lib/backframe/activerecord/acts_as_status.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 module Backframe
-  module ActsAsStatus
+  module ActsAsEnum
     extend ActiveSupport::Concern
 
     def self.included(base)
@@ -10,7 +10,7 @@ module Backframe
 
     module ClassMethods
 
-      def acts_as_status(*args)
+      def acts_as_enum(*args)
 
         field = args[0]
         arguments = args[1]
@@ -24,10 +24,10 @@ module Backframe
 
         if arguments.key?(:default)
 
-          after_initialize "init_#{field}_status".to_sym, :if => Proc.new { |d| d.new_record? }
+          after_initialize "init_#{field}".to_sym, :if => Proc.new { |d| d.new_record? }
 
           class_eval <<-EOV
-            def init_#{field}_status
+            def init_#{field}
               self.#{field} ||= '#{arguments[:default]}'
             end
           EOV
@@ -38,13 +38,20 @@ module Backframe
 
           validates_inclusion_of field, :in => arguments[:in]
 
-          arguments[:in].each do |status|
+          arguments[:in].each do |option|
             class_eval <<-EOV
-              scope :#{status}, -> { where(:status => '#{status}') }
+              scope :#{option}, -> { where(:#{field} => '#{option}') }
 
-              def #{status}?
-                self.#{field} == '#{status}'
+              def #{option}?
+                self.#{field} == '#{option}'
               end
+
+              scope :#{field}_#{option}, -> { where(:#{field} => '#{option}') }
+
+              def #{field}_#{option}?
+                self.#{field} == '#{option}'
+              end
+
             EOV
           end
 
