50709cf07203244c13d2576a99a55163baeafda3
Greg Kops <greg@thinktopography.com>
2016-10-06 22:23:25 -0400

    adding config for code climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
-require 'backframe'
-
 require "codeclimate-test-reporter"
+
+ENV['CODECLIMATE_REPO_TOKEN'] = "ff5eff2f4068518c0d38d88f8065b28a6b1513e55226f0a2a150ea84878cee2c"
 CodeClimate::TestReporter.start
+
+require 'backframe'
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
